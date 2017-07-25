f7c6beffe0a39d36882ec86bb3cce158a6c890fd
q15y5Blue <q15y5@outlook.com>
2017-07-25 10:42:34 +0800

    update Web

--- blog/craw/parser.py
+++ blog/craw/parser.py
@@ -4,18 +4,19 @@ from bs4 import BeautifulSoup
 from blog.models import Persons
 from .downloader import Downloader
 import re
-class Parser:
 
-    # 公用解析方法，返回soup对象
+
+class Parser:
+    # 公用解析方法,使用BeautifulSoup模块，返回soup对象
     def parse_url(self,url):
-        html_data = Downloader().download_html_by_url(url)
+        dt = Downloader()
+        html_data = dt.download_html_by_url(url)
         soup = BeautifulSoup(html_data, "html.parser")
         return soup
 
     # http://travel.qunar.com/space/follow/list?userId=158928832@qunar?page=1
     # 根据url下载 文档同时解析
-    def parse_data_followings(self, url, pageNo):
-        print(url)
+    def parse_data_followings(self, url):
         soup = self.parse_url(url)
         page = soup.find('ul', class_='fans-list')
         if page is None:
@@ -24,8 +25,63 @@ class Parser:
         per = Persons()
         per.name = soup.h3.span['title']
         per.describe = soup.find("span", class_='view-text').text
-        if(len(li)) > 0:
-            new_url = url+"?page=%s"%(pageNo)
-            pageNo=pageNo+1
-            self.parse_data_followings(new_url,pageNo)
-        return '123',"123123456"
+
+        # 获取关注的人数
+        follows_li = self.get_relation_data(url)
+        follow_list = []
+        for lis in follows_li:
+            for li in lis:
+                follow_list.append(li['data-id'])
+        per.set_following_list_to_str(follow_list)
+        per.followings_number = per.set_followings_number()
+        print(per.name, "关注的人数：", per.followings_number)
+
+        # 获取粉丝信息
+        url = url[:36] + "/fans" + url[36:]
+        fans_li = self.get_relation_data(url)
+        fans_list = []
+        for lis in fans_li:
+            for li in lis:
+                fans_list.append(li['data-id'])
+        per.set_fans_list_to_str(fans_list)
+        per.fans_number = per.set_fans_number()
+        print(per.name,"粉丝人数：",per.fans_number)
+        return '123', "123123456"
+
+
+    # 循环获取分页关注的人数
+    # http://travel.qunar.com/space/follow/list?userId=158928832@qunar&page=4
+    def get_relation_data(self, url):
+        li_list = []
+        for pageNo in range(1, 10000):
+            new_url = url+"&page=%s"%(pageNo)
+            print("获取分页关注人：", new_url)
+            pageNo += 1
+            soup = self.parse_url(new_url)
+            ul = soup.find('ul', class_='fans-list')
+            if ul is None:
+                break
+            li = ul.find_all('li')
+            if li is None or len(li) == 0:
+                break
+            li_list.append(li)
+        return li_list
+
+    # def get_fans_data(self, url):
+    #
+    #     li_list = []
+    #     for pageNo in range(1, 10000):
+    #         new_url = url+"&page=%s"%(pageNo)
+    #         print("获取分页粉丝:", new_url)
+    #         pageNo += 1
+    #         soup = self.parse_url(new_url)
+    #         ul = soup.find('ul', class_='fans-list')
+    #         if ul is None:
+    #             break
+    #         li = ul.find_all('li')
+    #         if li is None or len(li) == 0:
+    #             break
+    #         li_list.append(li)
+    #     return li_list
+
+
--- blog/models.py
+++ blog/models.py
@@ -9,26 +9,31 @@ from django.db import models
 # http://travel.qunar.com/space/158928832@qunar
 # http://travel.qunar.com/space/follow/list?userId=158928832&page=1
 class Persons(models.Model):
-    identify = models.CharField(max_length=32,default='user_identify_id')
-    name = models.CharField(max_length=32,default='user_name')
-    describe = models.CharField(max_length=1000,default='user_describe')
+    identify = models.CharField(max_length=32, default='user_identify_id')
+    name = models.CharField(max_length=32, default='user_name')
+    describe = models.CharField(max_length=1000, default='user_describe')
     followings = models.TextField()
     fans = models.TextField()
-    followings_number = models.IntegerField(max_length=16,default='user_followings_numbers')
-    fans_number = models.IntegerField(max_length=16,default='user_fans_numbers')
+    followings_number = models.IntegerField(max_length=16,default='user_followings_numbers')# 关注人数
+    fans_number = models.IntegerField(max_length=16,default='user_fans_numbers')             # 粉丝人数
     # 151310985,158928832,5931438
 
-    def getFollowingsToList(self):
+    def get_following_str_to_list(self):
         return self.followings.split(',')
-    def setListToFollowings(self,list):
-        self.describe = ','.join(list)
-    def getFansToList(self):
+
+    def set_following_list_to_str(self, list):
+        self.followings = ','.join(list)
+
+    def get_fans_str_to_list(self):
         return self.fans.split(',')
-    def setListToFans(self,list):
-        self.fans= ','.join(list)
+
+    def set_fans_list_to_str(self,list):
+        self.fans=','.join(list)
+
     # 获取关注的人数
-    def followings_number(self):
-        return len(self.getFollowingsToList())
+    def set_followings_number(self):
+        return len(self.get_following_str_to_list())
+
     # 获取粉丝人数
-    def fans_number(self):
-        return len(self.getFansToList())
+    def set_fans_number(self):
+        return len(self.get_fans_str_to_list())
